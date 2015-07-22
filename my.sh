6bde9e9c0787be362c004b1a84c9c47bc0c2386c
RyantSanders <ryantsanders13@gmail.com>
2015-07-22 17:07:55 -0500

    update
    update

--- my.sh
+++ my.sh
@@ -21,5 +21,22 @@
 # d=$(pwd)
 # echo $d
 
-a=$(ping -c 1 example.com |grep 'bytes from' | cut -d = -f 4)
-echo "The ping was $a"
+#a=$(ping -c 1 example.com |grep 'bytes from' | cut -d = -f 4)
+#echo "The ping was $a"
+
+#d=2
+#e=$((d+2))
+#echo $e
+#((e++))
+#echo $e
+#((e--))
+#echo $e
+#echo
+#((e+5=5))
+#echo $e
+#((e*=3))
+echo $e
+((e/=3))
+echo $e
+((e-=5))
+echo $e≈
