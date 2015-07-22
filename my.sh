2162b199047c1178cc33faf09b1c77c54975209b
RyantSanders <ryantsanders13@gmail.com>
2015-07-22 16:59:45 -0500

    update
    update

--- my.sh
+++ my.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # This is a basic bash script.
-greeting="hello"
+# greeting="hello"
 
-echo $greeting, world \(planet\)!
+# echo $greeting, world \(planet\)!
 
-echo '$greeting, world (planet)!'
+# echo '$greeting, world (planet)!'
 
-echo "$greeting, world (planet)!"
+# echo "$greeting, world (planet)!"
+
+# a=Hello
+# b="Good Morning"
+# c=16
+
+# echo $a
+# echo $b
+# echo $c
+
+# echo "$b! I have $c apples."
+
+# d=$(pwd)
+# echo $d
+
+a=$(ping -c 1 example.com |grep 'bytes from' | cut -d = -f 4)
+echo "The ping was $a"
