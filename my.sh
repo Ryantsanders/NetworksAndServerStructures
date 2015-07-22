6e5cc82c8510de9a4e89933aa90dc642cadc3fe0
RyantSanders <ryantsanders13@gmail.com>
2015-07-22 17:59:35 -0500

    update
    update

--- my.sh
+++ my.sh
@@ -54,3 +54,52 @@
 #b="cat"
 #[[ -z $a && -n $b ]]
 #echo $?
+
+#flashred="\033[5;31:40m"
+#red="\033[31;40m"
+#none="\033[0m"
+#echo -e $flashred"ERROR: "$none$red"Something went wrong."$none
+
+#flashred=$(tput setab 0; tput setaf 1; tput blink)
+#red=$(tput setab 0; tput setaf 1)
+#none=$(tput sgr0)
+#echo -e $flashred"ERROR: "$none$red"Something went wrong."$none
+
+#today=$(date +"%d-%m-%Y")
+#time=$(date +"%H:%M:%S")
+#printf -v d "Current User:\t%s\nDate:\t\t%s\n" $USER $today $time
+#echo "$d"
+
+#a=()
+#b=("apple" "banana" "cherry")
+#echo ${b[2]}
+#b[5]="kiwi"
+#b+=("mango")
+#echo ${b[@]}
+
+#declare -A myarray
+#myarray[color]=blue
+#myarray["office building"]="HQ West"
+
+#echo ${myarray['office building']} is ${myarray[color]}
+
+#i=1
+#while read f; do
+#	echo "Line $i: $f"
+#	((i++))
+#done < file.txt
+
+#cat <<- EndOfTExt
+#	this is a
+#	multiline
+#	text string
+#EndOfText
+
+ftp -n <<= Done WithTheUpdate
+	open mirrors.xmission.com
+	user anoymous nothinghere
+	ascii
+	cd gutenberg
+	get GUTINDEX.01
+	bye
+DoneWithTheUpdate
